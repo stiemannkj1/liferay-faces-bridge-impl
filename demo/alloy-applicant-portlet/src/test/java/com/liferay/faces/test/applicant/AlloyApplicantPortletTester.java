187ab4f76238d3a024b3b53ce66048fe5022a8f6
Kyle Stiemann <stiemannkj1@gmail.com>
2016-09-29 15:06:13 -0400

    Finished applicant tests

--- demo/alloy-applicant-portlet/src/test/java/com/liferay/faces/test/applicant/AlloyApplicantPortletTester.java
+++ demo/alloy-applicant-portlet/src/test/java/com/liferay/faces/test/applicant/AlloyApplicantPortletTester.java
@@ -15,6 +15,7 @@
  */
 package com.liferay.faces.test.applicant;
 
+import com.liferay.faces.test.selenium.Browser;
 import com.liferay.faces.test.selenium.TestUtil;
 import com.liferay.faces.test.selenium.applicant.ApplicantTesterBase;
 
@@ -28,16 +29,69 @@ public class AlloyApplicantPortletTester extends ApplicantTesterBase {
 	protected String getContext() {
 		return TestUtil.getSystemPropertyOrDefault("integration.context", "/group/bridge-demos/alloy-applicant");
 	}
-	
+
+	@Override
+	protected String getFieldErrorXpath(String fieldXpath) {
+
+		String errorMessageXpath =
+			"/span[contains(@class,'alloy-message')][contains(@class,'help-inline') or contains(@class,'help-block')]";
+
+		return "(" + fieldXpath + errorMessageXpath + "|" + fieldXpath + "/.." + errorMessageXpath + "|" + fieldXpath +
+			"/../.." + errorMessageXpath + ")";
+	}
+
+	@Override
+	protected String getPreferencesResetButtonXpath() {
+		return "//button[contains(@class,'btn')][contains(text(),'Reset')]";
+	}
+
+	@Override
+	protected String getPreferencesSubmitButtonXpath() {
+		return "//button[contains(@class,'btn')][contains(text(),'Submit')]";
+	}
+
+	@Override
+	protected String getSubmitAnotherApplicationButton() {
+		return "//button[contains(@class,'btn')][contains(text(),'Submit Another Application')]";
+	}
+
+	@Override
+	protected String getSubmitButtonXpath() {
+		return "//button[contains(@class,'btn')][contains(text(),'Submit')]";
+	}
+
 	// TODO
 	// alloy-applicant
-	// test tests on FF, Phantom, and chrome
+	// test tests on FF, Phantom,][chrome
 	// report icefaces bug with fileupload
 	// https://issues.liferay.com/browse/FACES-2867 No longer reproducable?
 	// Note version requirements:
-//	~ (master) $ chromedriver --version
-//	ChromeDriver 2.24.417412 (ac882d3ce7c0d99292439bf3405780058fcca0a6)
-//	~ (master) $ phantomjs --version
-//	2.1.1
-//	Firefox (see if we can upgrade to recent version)
+// ~ (master) $ chromedriver --version
+// ChromeDriver 2.24.417412 (ac882d3ce7c0d99292439bf3405780058fcca0a6)
+// ~ (master) $ phantomjs --version
+// 2.1.1
+// Firefox (see if we can upgrade to recent version)
+
+	@Override
+	protected String getSubmitFileButtonXpath() {
+		return
+			"//form[@method='post'][@enctype='multipart/form-data']/button[contains(@class,'btn')][contains(text(),'Submit')]";
+	}
+
+	@Override
+	protected String getUploadedFileXpath() {
+		return
+			"//h3[contains(text(),'Attachments')]/following-sibling::table[contains(@class,'alloy-data-table')]/tbody/tr/td[2]";
+	}
+
+	@Override
+	protected void selectDate(Browser browser) {
+
+		browser.click("//button[contains(@id,'dateOfBirth')]/span[contains(@class,'calendar')][not(text())]");
+
+		String dateElementXpath =
+			"//table[contains(@class, 'yui3-calendar-grid')]//td[contains(text(),'14')][not(contains(@class,'yui3-calendar-column-hidden'))]";
+		browser.waitForElementVisible(dateElementXpath);
+		browser.click(dateElementXpath);
+	}
 }
