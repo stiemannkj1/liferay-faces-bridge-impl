2b5eed8ebe9efbd35e1cbcddfdf4cbbd42840817
Kyle Stiemann <stiemannkj1@gmail.com>
2016-10-03 18:28:09 -0400

    --amend

--- test/integration/src/test/java/com/liferay/faces/bridge/test/integration/BridgeTestUtil.java
+++ test/integration/src/test/java/com/liferay/faces/bridge/test/integration/BridgeTestUtil.java
@@ -11,7 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
-package com.liferay.faces.bridge.test.integration.demo;
+package com.liferay.faces.bridge.test.integration;
 
 import com.liferay.faces.test.selenium.TestUtil;
 
